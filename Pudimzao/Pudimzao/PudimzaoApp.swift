6f4ce0333610929ebcb675df64b8e6c35139dd29
WillianAlbeche <willianalbeche@gmail.com>
2021-11-29 14:24:38 -0300

    first Commit

---
+++ Pudimzao/Pudimzao/PudimzaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PudimzaoApp.swift
+//  Pudimzao
+//
+//  Created by Willian Magnum Albeche on 29/11/21.
+//
+
+import SwiftUI
+
+@main
+struct PudimzaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
