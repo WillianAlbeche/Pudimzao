73b833443b075d28e09b32cc566f78099910e15f
WillianAlbeche <willianalbeche@gmail.com>
2021-11-30 17:03:06 -0300

    Animation done

--- Pudimzao/Pudimzao/ContentView.swift
+++ Pudimzao/Pudimzao/ContentView.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var percent: CGFloat = 0
+   
+    
     var body: some View {
-        Text("hello world")
+        VStack {
+            
+                    
+                RingView(percent: percent)
+            
+            HStack{
+                Button{
+                    percent = 100
+                } label: {
+                    Text("Finish")
+                }
+                
+                Button{
+                    self.startLoading()
+                } label: {
+                    Text("Start")
+                }
+            }
+        }
+    }
+    
+    func startLoading() {
+        _ = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+            withAnimation() {
+                self.percent += 1
+                if self.percent >= 100.0 {
+                    timer.invalidate()
+                }
+            }
+        }
     }
 }
 
--- Pudimzao/Pudimzao/RingView.swift
+++ Pudimzao/Pudimzao/RingView.swift
@@ -8,14 +8,63 @@
 import SwiftUI
 
 struct RingView: View {
+    @State private var showingAlert = false
+    var percent: CGFloat = 0
     var body: some View {
-        Circle()
-                .trim(from: 0.2, to: 1)
-                .stroke(
-                        LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1))]), startPoint: .topTrailing, endPoint: .bottomLeading),
-                        style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20, 0], dashPhase: 0)
+        
+        ZStack {
+            
+            Circle()
+                .foregroundColor(Color(#colorLiteral(red: 0.1892106831, green: 0.1867186129, blue: 0.2563015223, alpha: 1)))
+           
+                .overlay(
+                    Circle()
+                        .trim(from: 0, to: 1)
+                        .stroke(style: StrokeStyle(lineWidth: 50, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20, 0], dashPhase: 0)
+                               )
+                    
+                        .stroke(style: StrokeStyle(lineWidth: 30, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20, 0], dashPhase: 0)
+                               )
                 )
-                .frame(width: 44, height: 44)    }
+                .opacity(0.20)
+                .frame(width: 190, height: 190)
+            
+            
+            Circle()
+                .fill(Color.white)
+                .frame(width: 260, height: 260)
+                .overlay(
+                    Circle()
+                        .trim(from: 0, to: percent * 0.01)
+                        .stroke(style: StrokeStyle(lineWidth: 15, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20, 0], dashPhase: 0)
+                               )
+                        .fill(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.9942818284, green: 0, blue: 0.5605643392, alpha: 1)), Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1))]), startPoint: .topTrailing, endPoint: .bottomLeading))
+                ).animation(.spring(response: 1.0, dampingFraction: 1, blendDuration: 10))
+            Text("pudimzando voce ").font(.system(size: 20))
+            
+            if percent == 100 {
+                Button("Show Alert") {
+                    showingAlert = true
+                }
+                .alert("Voce foi pudimzado", isPresented: $showingAlert) {
+                    Button("OK", role: .cancel) { }
+                }
+            }
+        }
+        
+        
+        
+        
+        
+        //                .rotationEffect(Angle(degrees: 90))
+        //                .rotation3DEffect(Angle(degrees: 180), axis: (x: 1, y: 0, z: 0))
+        //.frame(width: 120, height: 120)
+        //.animation(.spring(response: 1.0, dampingFraction: 1, blendDuration: 1))
+        
+    }
+    
+    
+    
 }
 
 struct RingView_Previews: PreviewProvider {
