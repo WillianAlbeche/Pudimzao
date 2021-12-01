c0d287dec62c6f025c7db66fa54407f8937634df
WillianAlbeche <willianalbeche@gmail.com>
2021-12-01 14:09:59 -0300

    Merge branch 'main' into AnimationLoading

--- Pudimzao/Pudimzao/Views/MainView.swift
+++ Pudimzao/Pudimzao/Views/MainView.swift
@@ -8,9 +8,40 @@
 import SwiftUI
 
 struct MainView: View {
+    
+    @State var percent: CGFloat = 0
     var body: some View {
-        Text("voce é o pudim do(a) \(getPudimType())")
-            .foregroundColor(.white)
+        VStack {
+            Text("voce é o pudim do(a) \(getPudimType())")
+                .foregroundColor(.white)
+            RingView(percent: percent)
+            
+            HStack{
+                Button{
+                    percent = 100
+                } label: {
+                    Text("Finish")
+                }
+                
+                Button{
+                    self.startLoading()
+                } label: {
+                    Text("Start")
+                }
+            }
+        }
+    }
+    
+    
+    func startLoading() {
+        _ = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+            withAnimation() {
+                self.percent += 1
+                if self.percent >= 100.0 {
+                    timer.invalidate()
+                }
+            }
+        }
     }
 }
 
