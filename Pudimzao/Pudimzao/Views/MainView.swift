80b5a914d5282401d0eaf5c65eca61e071aab5bb
WillianAlbeche <willianalbeche@gmail.com>
2021-12-03 14:00:21 -0300

    correcao de bugs

--- Pudimzao/Pudimzao/Views/MainView.swift
+++ Pudimzao/Pudimzao/Views/MainView.swift
@@ -10,44 +10,22 @@ import SwiftUI
 struct MainView: View {
     var body: some View {
         VStack{
-        Text("voce é o pudim do(a) \(getPudimType())")
-            .foregroundColor(.white)
-        RingView()
-<<<<<<< HEAD
-        VStack {
             Text("voce é o pudim do(a) \(getPudimType())")
                 .foregroundColor(.white)
-            RingView(percent: percent)
+            //RingView()
             
-            HStack{
-                Button{
-                    percent = 100
-                } label: {
-                    Text("Finish")
-                }
+            VStack {
+                Text("voce é o pudim do(a) \(getPudimType())")
+                    .foregroundColor(.white)
+                RingView()
                 
-                Button{
-                    self.startLoading()
-                } label: {
-                    Text("Start")
-                }
             }
         }
+        
+        
     }
+  
     
-    
-    func startLoading() {
-        _ = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
-            withAnimation() {
-                self.percent += 1
-                if self.percent >= 100.0 {
-                    timer.invalidate()
-                }
-            }
-=======
->>>>>>> parent of 24d26bc (sinceramente n me lembro)
-        }
-    }
 }
 
 struct MainView_Previews: PreviewProvider {
--- Pudimzao/Pudimzao/Views/RingView.swift
+++ Pudimzao/Pudimzao/Views/RingView.swift
@@ -8,14 +8,73 @@
 import SwiftUI
 
 struct RingView: View {
+    @State var percent: CGFloat = 0
+    @State private var showingAlert = false
     var body: some View {
-        Circle()
-                .trim(from: 0.2, to: 1)
-                .stroke(
-                        LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1))]), startPoint: .topTrailing, endPoint: .bottomLeading),
-                        style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20, 0], dashPhase: 0)
-                )
-                .frame(width: 44, height: 44)    }
+        
+        VStack {
+            ZStack {
+                
+                Circle()
+                    .foregroundColor(Color(#colorLiteral(red: 0.1892106831, green: 0.1867186129, blue: 0.2563015223, alpha: 1)))
+               
+                    .overlay(
+                        Circle()
+                            .trim(from: 0, to: 1)
+                            .stroke(style: StrokeStyle(lineWidth: 50, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20, 0], dashPhase: 0)
+                                   )
+                        
+                            .stroke(style: StrokeStyle(lineWidth: 30, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20, 0], dashPhase: 0)
+                                   )
+                    )
+                    .opacity(0.20)
+                    .frame(width: 190, height: 190)
+                
+                
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: 260, height: 260)
+                    .overlay(
+                        Circle()
+                            .trim(from: 0, to: percent * 0.01)
+                            .stroke(style: StrokeStyle(lineWidth: 15, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20, 0], dashPhase: 0)
+                                   )
+                            .fill(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.9942818284, green: 0, blue: 0.5605643392, alpha: 1)), Color(#colorLiteral(red: 1, green: 0.8352941176, blue: 0.6235294118, alpha: 1))]), startPoint: .topTrailing, endPoint: .bottomLeading))
+                    ).animation(.spring(response: 1.0, dampingFraction: 1, blendDuration: 10))
+                Text("pudimzando voce ").font(.system(size: 20))
+                
+            }
+            HStack{
+                
+                Button{
+                    percent = 100
+                    
+                } label: {
+                    Text("Finish")
+                }
+                
+                Button{
+                    self.startLoading()
+                    
+                } label: {
+                    Text("Start")
+                }
+            }
+        }
+        
+    }
+    
+    
+    func startLoading() {
+        _ = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+            withAnimation() {
+                self.percent += 1
+                if self.percent >= 100.0 {
+                    timer.invalidate()
+                }
+            }
+        }
+    }
 }
 
 struct RingView_Previews: PreviewProvider {
